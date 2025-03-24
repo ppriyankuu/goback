21ed225954cd50776a7db17df74c99629d2f56e6
Priyanku Gogoi <workwithpriyanku@gmail.com>
2025-03-24 19:54:41 +0530

    documented permissions.go

--- internals/fs/permissions.go
+++ internals/fs/permissions.go
@@ -2,30 +2,27 @@ package fs
 
 import (
 	"fmt"
-	"os"
-	"syscall"
 )
 
+// PreservePermissions copies the file permissions, timestamps and ownership
+// from the source file to the destination file.
+
+// Parameters:
+// - source: The path of the source file whose metadata need to be copied.
+// - destination: The path of the destination file to which metadata is applied.
+
+// Returns:
+// - error: An error if any step in preserving metadata fails.
 func PreservePermissions(source, destination string) error {
-	srcInfo, err := os.Stat(source)
+	// Retrive metadata of the source file.
+	srcInfo, err := GetFileMetadata(source)
 	if err != nil {
 		return fmt.Errorf("failed to get source file info: %w", err)
 	}
 
-	if err := os.Chmod(destination, srcInfo.Mode()); err != nil {
-		return fmt.Errorf("failed to set destination file mode: %w", err)
-	}
-	if err := os.Chtimes(destination, srcInfo.ModTime(), srcInfo.ModTime()); err != nil {
-		return fmt.Errorf("failed to set destination file times: %w", err)
-	}
-
-	stat, ok := srcInfo.Sys().(*syscall.Stat_t)
-	if !ok {
-		return fmt.Errorf("failed to retrieve file ownership information")
-	}
-
-	if err := os.Lchown(destination, int(stat.Uid), int(stat.Gid)); err != nil {
-		return fmt.Errorf("failed to set destination file ownership: %w", err)
+	// Set the metadata on the destination file.
+	if err := SetFileMetadata(destination, srcInfo); err != nil {
+		return fmt.Errorf("failed to preserve permissions: %w", err)
 	}
 
 	return nil
