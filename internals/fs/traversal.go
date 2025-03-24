90973c559e230a2b049c038b641039fa8de1281e
Priyanku Gogoi <workwithpriyanku@gmail.com>
2025-03-24 19:54:56 +0530

    documented traversal.go

--- internals/fs/traversal.go
+++ internals/fs/traversal.go
@@ -6,13 +6,26 @@ import (
 	"path/filepath"
 )
 
+// TraversalDirectory walks through the given directory and collects
+// all file paths (excluding directories)
+
+// Parameters:
+// - dir: The directory path to traverse.
+
+// Returns:
+// - []string: A slice containing the paths of all files found.
+// - error: An error if the traversal fails.
 func TraversalDirectory(dir string) ([]string, error) {
+	// Initialise an empty list to store the files.
 	var files []string
+
+	// Walk the directory and process each file or folder.
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
+		// If it's not a directory, add the file path to the list
 		if !info.IsDir() {
 			files = append(files, path)
 		}
@@ -24,5 +37,6 @@ func TraversalDirectory(dir string) ([]string, error) {
 		return nil, fmt.Errorf("failed to traverse directory: %w", err)
 	}
 
+	// Return the list of the file paths
 	return files, nil
 }
