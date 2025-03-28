cb8664a8af872b1c9f7ad216fc5f87b041a3415e
Priyanku Gogoi <workwithpriyanku@gmail.com>
2025-03-28 09:32:06 +0530

    added comments for the progress tracking logic

--- internals/cli/progress.go
+++ internals/cli/progress.go
@@ -2,7 +2,12 @@ package cli
 
 import "fmt"
 
-func TrackProgress(message string, args ...interface{}) {
+// TrackProgress logs the progress of an operation with a formatted message.
+
+// Parameters:
+// - message: The format string for the message to be logged.
+// - args: Additional arguments to format the message.
+func TrackProgress(message string, args ...any) {
 	fmt.Printf(message, args...)
 	fmt.Println()
 }
